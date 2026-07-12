2e62ad9595925a88538782ee1ea66f84160f4428
Ashutosh Upadhyay <ashutoshupadhyay9522@gmail.com>
2026-07-12 17:16:02 +0530

    updated

--- backend/src/controllers/resetcontroller.js
+++ backend/src/controllers/resetcontroller.js
@@ -1,27 +1,14 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/register");
-const OTP = require("../models/otpmodel");
 
 const resetPassword = async (req, res) => {
-
     try {
+        const { password, confirmPassword } = req.body;
 
-        const {
-            email,
-            otp,
-            password,
-            confirmPassword
-        } = req.body;
-
-        if (
-            !email ||
-            !otp ||
-            !password ||
-            !confirmPassword
-        ) {
+        if (!password || !confirmPassword) {
             return res.status(400).json({
                 success: false,
-                message: "All fields are required"
+                message: "Password and Confirm Password are required"
             });
         }
 
@@ -32,42 +19,13 @@ const resetPassword = async (req, res) => {
             });
         }
 
-        const otpRecord = await OTP.findOne({ email });
-              console.log(otpRecord);
-        if (!otpRecord) {
-            return res.status(400).json({
-                success: false,
-                message: "OTP not found"
-            });
-        }
-
-        if (otpRecord.expiresAt < new Date()) {
-
-            await OTP.deleteOne({ email });
-
-            return res.status(400).json({
-                success: false,
-                message: "OTP expired"
-            });
-        }
-
-        if (otpRecord.otp !== otp) {
-            return res.status(400).json({
-                success: false,
-                message: "Invalid OTP"
-            });
-        }
-
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        await User.findOneAndUpdate(
-            { email },
-            {
-                password: hashedPassword
-            }
+        await User.findByIdAndUpdate(
+            req.user.id,
+            { password: hashedPassword },
+            { new: true }
         );
-
-        await OTP.deleteOne({ email });
 
         return res.status(200).json({
             success: true,
@@ -75,16 +33,13 @@ const resetPassword = async (req, res) => {
         });
 
     } catch (error) {
-
         console.log(error);
 
         return res.status(500).json({
             success: false,
             message: "Internal Server Error"
         });
-
     }
-
 };
 
-module.exports ={ resetPassword};
+module.exports = { resetPassword };
