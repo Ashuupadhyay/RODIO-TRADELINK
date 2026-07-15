6e3b290cc3a42fb5bba2968a1694d1c8f4840828
Ashutosh Upadhyay <ashutoshupadhyay9522@gmail.com>
2026-07-15 14:27:47 +0530

    updtef

--- backend/src/controllers/profileController.js
+++ backend/src/controllers/profileController.js
@@ -1,5 +1,91 @@
 const Profile = require("../models/profile");
 const User = require("../models/register");
+const cloudinary = require("../config/cloudnary");
+const streamifier = require("streamifier");
+
+
+//poat 
+
+
+// UPDATE PROFILE
+
+const updateProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const { name, phoneNumber } = req.body;
+
+    // Registered User
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    let imageUrl = "";
+
+    // Image Upload
+    if (req.file) {
+      const uploadImage = () =>
+        new Promise((resolve, reject) => {
+          const stream = cloudinary.uploader.upload_stream(
+            {
+              folder: "profiles",
+            },
+            (error, result) => {
+              if (error) reject(error);
+              else resolve(result);
+            }
+          );
+
+          streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+
+      const result = await uploadImage();
+
+      imageUrl = result.secure_url;
+    }
+
+    // Profile Find
+    let profile = await Profile.findOne({ user: userId });
+
+    if (!profile) {
+      profile = await Profile.create({
+        user: userId,
+        role: user.role,
+        name: name || user.name,
+        email: user.email,
+        phoneNumber: phoneNumber || user.mobile,
+        profileImage: imageUrl,
+      });
+    } else {
+      if (name) profile.name = name;
+
+      if (phoneNumber) profile.phoneNumber = phoneNumber;
+
+      if (imageUrl) profile.profileImage = imageUrl;
+
+      await profile.save();
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Profile Updated Successfully",
+      profile,
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
 // GET API
 // Default Profile Image
 const DEFAULT_PROFILE_IMAGE =
@@ -59,4 +145,6 @@ const getProfile = async (req, res) => {
 
 module.exports = {
   getProfile,
+  updateProfile,
+
 };
--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-
+const upload = require("../middlewhere/multer");
 const auth = require("../middlewhere/auth");
 
-const { getProfile } = require("../controllers/profileController");
+const { getProfile, updateProfile } = require("../controllers/profileController");
 
 router.get("/", auth, getProfile);
-
+router.put("/", auth, upload.single("profileImage"), updateProfile);
 module.exports = router;
