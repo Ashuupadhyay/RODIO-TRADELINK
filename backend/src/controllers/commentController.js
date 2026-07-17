87e04411068a0bdd6b957652c411be84b2d1e796
Ashutosh Upadhyay <ashutoshupadhyay9522@gmail.com>
2026-07-17 16:13:28 +0530

    fjfh

--- backend/src/controllers/commentController.js
+++ backend/src/controllers/commentController.js
@@ -2,10 +2,8 @@ const Comment = require("../models/comments");
 const Business = require("../models/business");
 const Profile = require("../models/profile");
 
-
 const addComment = async (req, res) => {
   try {
-
     const transporter = await Business.findById(req.params.id);
 
     if (!transporter) {
@@ -15,10 +13,11 @@ const addComment = async (req, res) => {
       });
     }
 
-    const profile = await Profile.findOne({
+    await Profile.findOne({
       user: req.user.id,
     });
 
+    // Create Review
     const review = await Comment.create({
       transporter: transporter._id,
       user: req.user.id,
@@ -26,21 +25,35 @@ const addComment = async (req, res) => {
       comment: req.body.comment,
     });
 
+    // Push review into transporter
+    transporter.comments.push(review._id);
+
+    // Get all reviews of transporter
+    const reviews = await Comment.find({
+      transporter: transporter._id,
+    });
+
+    transporter.totalReviews = reviews.length;
+
+    transporter.averageRating =
+      reviews.reduce((sum, item) => sum + item.rating, 0) /
+      reviews.length;
+
+    await transporter.save();
+
     res.status(201).json({
       success: true,
       review,
     });
 
   } catch (error) {
-
     res.status(500).json({
       success: false,
       message: error.message,
     });
-
   }
 };
 
 module.exports = {
-    addComment
-}
+  addComment,
+};
