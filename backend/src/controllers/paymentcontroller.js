8eaffeb1758b3be467dfb14b6ac8eb64a46205ba
Ashutosh Upadhyay <ashutoshupadhyay9522@gmail.com>
2026-07-21 16:19:53 +0530

    ffgg

--- backend/src/controllers/paymentcontroller.js
+++ backend/src/controllers/paymentcontroller.js
@@ -1,9 +1,30 @@
+const razorpay = require("../config/razorpay");
+const Payment = require("../models/Payment");
+
 exports.createOrder = async (req, res) => {
   try {
-    res.status(200).json({
+    const { amount } = req.body;
+
+    if (!amount) {
+      return res.status(400).json({
+        success: false,
+        message: "Amount is required",
+      });
+    }
+
+    const options = {
+      amount: amount * 100, // Paisa
+      currency: "INR",
+      receipt: `receipt_${Date.now()}`,
+    };
+
+    const order = await razorpay.orders.create(options);
+
+    res.status(201).json({
       success: true,
-      message: "Create Order API Working",
+      order,
     });
+
   } catch (error) {
     res.status(500).json({
       success: false,
