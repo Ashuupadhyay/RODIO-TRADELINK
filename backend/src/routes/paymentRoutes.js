65f8c1f11e681f1cbdcf4b9d498049c582e9f93f
Ashutosh Upadhyay <ashutoshupadhyay9522@gmail.com>
2026-08-02 16:38:02 +0530

    fkhbfbvhbf

--- backend/src/routes/paymentRoutes.js
+++ backend/src/routes/paymentRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { createOrder,verifyPayment,getReceipt} = require("../controllers/paymentcontroller");
+const { createOrder,verifyPayment,getReceipt,getReferralStats} = require("../controllers/paymentcontroller");
 const auth = require("../middlewhere/auth");
 // Create Order API
 router.post("/create-order", auth,createOrder);
 router.post("/verify-payment", auth, verifyPayment);
 router.get("/receipt/:paymentId", auth, getReceipt);
+router.get("/referral/stats", auth, getReferralStats);
 
 module.exports = router;
