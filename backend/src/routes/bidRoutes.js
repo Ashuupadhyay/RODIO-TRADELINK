3e6d643588c8f43f5ac5f87820f0707b1b8924e3
Ashutosh Upadhyay <ashutoshupadhyay9522@gmail.com>
2026-08-26 12:35:57 +0530

    ckjnjsfnjn

--- backend/src/routes/bidRoutes.js
+++ backend/src/routes/bidRoutes.js
@@ -1,40 +1,149 @@
 const express = require("express");
 
+
+
 const router = express.Router();
 
-const { createBid,getLeadBids,acceptBid,  updateBid,deleteBid } = require("../controllers/bidController");
+
+
+const {
+
+
+
+    createBid,
+
+
+
+    getLeadBids,
+
+
+
+    acceptBid,
+
+
+
+    myBids,
+
+
+
+    updateBid,
+
+
+
+    deleteBid
+
+
+
+} = require("../controllers/bidController");
+
+
 
 const auth = require("../middlewhere/auth");
 
+
+
 router.post("/create/:bookingId", auth, createBid);
 
-router.get(
-    "/booking/:bookingId",
-    auth,
-    getLeadBids
-);
-router.put(
-    "/accept/:bidId",
-    auth,
-    acceptBid
-);
+
 
 router.get(
-    "/my-bids",
-    auth,
-    myBids
-);
-// Transporter apni Pending bid edit kare
-router.put(
-    "/update/:bidId",
-    auth,
-    updateBid
+
+
+
+    "/booking/:bookingId",
+
+
+
+    auth,
+
+
+
+    getLeadBids
+
+
+
 );
 
-// Transporter apni Pending bid delete kare
+
+
+router.put(
+
+
+
+    "/accept/:bidId",
+
+
+
+    auth,
+
+
+
+    acceptBid
+
+
+
+);
+
+
+
+router.get(
+
+
+
+    "/my-bids",
+
+
+
+    auth,
+
+
+
+    myBids
+
+
+
+);
+
+
+
+router.put(
+
+
+
+    "/update/:bidId",
+
+
+
+    auth,
+
+
+
+    updateBid
+
+
+
+);
+
+
+
 router.delete(
-    "/delete/:bidId",
-    auth,
-    deleteBid
+
+
+
+    "/delete/:bidId",
+
+
+
+    auth,
+
+
+
+    deleteBid
+
+
+
 );
+
+
+
 module.exports = router;
